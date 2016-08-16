67b2d8ef560645aaf39f0c82caf48b2940e5f60b
Pelle Wessman <pelle@kodfabrik.se>
2016-08-16 20:40:21 +0200

    Added text for complex condition

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -2,22 +2,104 @@
 
 const chai = require('chai');
 
-const should = chai.should();
+chai.should();
 
 describe('Parser', function () {
   const compileCondition = require('../').compileCondition;
 
   it('should be able to parse a simple condition', () => {
-    let condition;
-
-    should.not.Throw(() => {
-      condition = compileCondition('foo = bar');
-    });
-
-    condition.should.deep.equal({
+    const condition = 'foo = bar';
+    compileCondition(condition).should.deep.equal({
       operator: '==',
       property: ['foo'],
       value: 'bar'
+    });
+  });
+
+  it('should be able to parse a complex condition', () => {
+    const condition = '!(bookmark.bar-car.foo["abc adf sdaf sda f"][][][]=123) AND foo OR bar=abc OR foo="\\\\" OR foo[]=123 OR foo.abc[].bar.foo[] != true';
+    compileCondition(condition).should.deep.equal({
+      left: {
+        left: {
+          not: {
+            property: [
+              'bookmark',
+              'bar-car',
+              'foo',
+              'abc adf sdaf sda f',
+              {
+                array: true,
+                property: [{
+                  array: true,
+                  property: [{
+                    array: true
+                  }]
+                }]
+              }
+            ],
+            operator: '==',
+            value: 123
+          }
+        },
+        operator: 'AND',
+        right: {
+          property: [
+            'foo'
+          ]
+        }
+      },
+      operator: 'OR',
+      right: {
+        left: {
+          property: [
+            'bar'
+          ],
+          operator: '==',
+          value: 'abc'
+        },
+        operator: 'OR',
+        right: {
+          left: {
+            property: [
+              'foo'
+            ],
+            operator: '==',
+            value: '\\'
+          },
+          operator: 'OR',
+          right: {
+            left: {
+              property: [
+                'foo',
+                {
+                  array: true
+                }
+              ],
+              operator: '==',
+              value: 123
+            },
+            operator: 'OR',
+            right: {
+              property: [
+                'foo',
+                'abc',
+                {
+                  array: true,
+                  property: [
+                    'bar',
+                    'foo',
+                    {
+                      array: true
+                    }
+                  ]
+                }
+              ],
+              operator: '!=',
+              value: true
+            }
+          }
+        }
+      }
     });
   });
 });
