4c1c649e4009513607bf1fcc3c1ce867ff7166f7
Pelle Wessman <pelle@kodfabrik.se>
2016-08-17 20:18:01 +0200

    Grouped the test

--- test/fulfills.spec.js
+++ test/fulfills.spec.js
@@ -4,7 +4,7 @@ const chai = require('chai');
 
 chai.should();
 
-describe('Parser', function () {
+describe('Fulfills', function () {
   const fulfills = require('../');
 
   let testObject;
@@ -18,62 +18,64 @@ describe('Parser', function () {
     };
   });
 
-  it('should be able match a simple condition', () => {
-    fulfills(testObject, {
-      operator: '===',
-      property: ['foo'],
-      value: 'bar'
-    }).should.be.ok;
-  });
+  describe('comparison operators', () => {
+    it('should be able match a simple condition', () => {
+      fulfills(testObject, {
+        operator: '===',
+        property: ['foo'],
+        value: 'bar'
+      }).should.be.ok;
+    });
 
-  it('should be able match a deep condition', () => {
-    fulfills(testObject, {
-      operator: '===',
-      property: ['abc', 'def', 'ghi'],
-      value: 'bar'
-    }).should.be.ok;
-  });
+    it('should be able match a deep condition', () => {
+      fulfills(testObject, {
+        operator: '===',
+        property: ['abc', 'def', 'ghi'],
+        value: 'bar'
+      }).should.be.ok;
+    });
 
-  it('should be able match a "===" condition', () => {
-    fulfills(testObject, { operator: '===', property: ['num'], value: 123 }).should.be.ok;
-    fulfills(testObject, { operator: '===', property: ['num'], value: '123' }).should.not.be.ok;
-  });
+    it('should be able match a "===" condition', () => {
+      fulfills(testObject, { operator: '===', property: ['num'], value: 123 }).should.be.ok;
+      fulfills(testObject, { operator: '===', property: ['num'], value: '123' }).should.not.be.ok;
+    });
 
-  it('should be able match a "!==" condition', () => {
-    fulfills(testObject, { operator: '!==', property: ['num'], value: 123 }).should.not.be.ok;
-    fulfills(testObject, { operator: '!==', property: ['num'], value: '123' }).should.be.ok;
-  });
+    it('should be able match a "!==" condition', () => {
+      fulfills(testObject, { operator: '!==', property: ['num'], value: 123 }).should.not.be.ok;
+      fulfills(testObject, { operator: '!==', property: ['num'], value: '123' }).should.be.ok;
+    });
 
-  it('should be able match a "==" condition', () => {
-    fulfills(testObject, { operator: '==', property: ['num'], value: 123 }).should.be.ok;
-    fulfills(testObject, { operator: '==', property: ['num'], value: '123' }).should.be.ok;
-  });
+    it('should be able match a "==" condition', () => {
+      fulfills(testObject, { operator: '==', property: ['num'], value: 123 }).should.be.ok;
+      fulfills(testObject, { operator: '==', property: ['num'], value: '123' }).should.be.ok;
+    });
 
-  it('should be able match a "!=" condition', () => {
-    fulfills(testObject, { operator: '!=', property: ['num'], value: 123 }).should.not.be.ok;
-    fulfills(testObject, { operator: '!=', property: ['num'], value: '123' }).should.not.be.ok;
-  });
+    it('should be able match a "!=" condition', () => {
+      fulfills(testObject, { operator: '!=', property: ['num'], value: 123 }).should.not.be.ok;
+      fulfills(testObject, { operator: '!=', property: ['num'], value: '123' }).should.not.be.ok;
+    });
 
-  it('should be able match a "<" condition', () => {
-    fulfills(testObject, { operator: '<', property: ['num'], value: 200 }).should.be.ok;
-  });
+    it('should be able match a "<" condition', () => {
+      fulfills(testObject, { operator: '<', property: ['num'], value: 200 }).should.be.ok;
+    });
 
-  it('should be able match a ">" condition', () => {
-    fulfills(testObject, { operator: '>', property: ['num'], value: 100 }).should.be.ok;
-  });
+    it('should be able match a ">" condition', () => {
+      fulfills(testObject, { operator: '>', property: ['num'], value: 100 }).should.be.ok;
+    });
 
-  it('should be able match a "<=" condition', () => {
-    fulfills(testObject, { operator: '<=', property: ['num'], value: 123 }).should.be.ok;
-    fulfills(testObject, { operator: '<=', property: ['num'], value: 200 }).should.be.ok;
-  });
+    it('should be able match a "<=" condition', () => {
+      fulfills(testObject, { operator: '<=', property: ['num'], value: 123 }).should.be.ok;
+      fulfills(testObject, { operator: '<=', property: ['num'], value: 200 }).should.be.ok;
+    });
 
-  it('should be able match a ">=" condition', () => {
-    fulfills(testObject, { operator: '>=', property: ['num'], value: 123 }).should.be.ok;
-    fulfills(testObject, { operator: '>=', property: ['num'], value: 100 }).should.be.ok;
-  });
+    it('should be able match a ">=" condition', () => {
+      fulfills(testObject, { operator: '>=', property: ['num'], value: 123 }).should.be.ok;
+      fulfills(testObject, { operator: '>=', property: ['num'], value: 100 }).should.be.ok;
+    });
 
-  it('should handle an operator less condition', () => {
-    fulfills(testObject, { property: ['foo'] }).should.be.ok;
-    fulfills(testObject, { property: ['falsk'] }).should.not.be.ok;
+    it('should handle an operator less condition', () => {
+      fulfills(testObject, { property: ['foo'] }).should.be.ok;
+      fulfills(testObject, { property: ['falsk'] }).should.not.be.ok;
+    });
   });
 });
