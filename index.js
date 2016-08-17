2a45f5303b9e1a6ccd1e70f3512c1d10b48840a9
Pelle Wessman <pelle@kodfabrik.se>
2016-08-17 19:58:39 +0200

    Initial mapping of conditions

--- index.js
+++ index.js
@@ -6,8 +6,41 @@ const compileCondition = function (query) {
   return queryParser.parse(query);
 };
 
-const matchCondition = function () {
+const matchValueAgainstCondition = function (value, condition) {
+  if (condition.operator === '==') {
+    return value == condition.value; // eslint-disable-line
+  } else if (condition.operator === '!=') {
+    return value != condition.value; // eslint-disable-line
+  } else {
+    throw new Error('unimplemented operator "' + condition.operator + '"');
+  }
+};
+
+const matchIndividualCondition = function (obj, remainingProperty, condition) {
+  remainingProperty = [].concat(remainingProperty);
+
+  const key = remainingProperty.shift();
+
+  if (typeof key === 'object') {
+    // TODO: It's probably an array – deal with it!
+    throw new Error('Unimplemented key type');
+  } else if (typeof key !== 'string') {
+    throw new Error('Unknown key type');
+  }
+
+  if (obj[key] && remainingProperty.length) {
+    return matchIndividualCondition(obj[key], remainingProperty, condition);
+  }
+
+  return matchValueAgainstCondition(obj[key], condition);
+};
+
+const matchCondition = function (obj, condition) {
   // TODO: Match the condition against the object
+  if (condition.property) {
+    return matchIndividualCondition(obj, condition.property, condition);
+  }
+  throw new Error('Unimplemented condition structure');
 };
 
 const fulfills = function (obj, condition) {
--- test/fulfills.spec.js
+++ test/fulfills.spec.js
@@ -5,7 +5,38 @@ const chai = require('chai');
 chai.should();
 
 describe('Parser', function () {
-  // const fulfills = require('../');
+  const fulfills = require('../');
 
-  it('should be able match a simple condition');
+  let testObject;
+
+  before(() => {
+    testObject = {
+      foo: 'bar',
+      abc: { def: { ghi: 'bar' } }
+    };
+  });
+
+  it('should be able match a simple condition', () => {
+    fulfills(testObject, {
+      operator: '==',
+      property: ['foo'],
+      value: 'bar'
+    }).should.be.ok;
+  });
+
+  it('should be able match a deep condition', () => {
+    fulfills(testObject, {
+      operator: '==',
+      property: ['abc', 'def', 'ghi'],
+      value: 'bar'
+    }).should.be.ok;
+  });
+
+  it('should be able match a "!=" condition', () => {
+    fulfills(testObject, {
+      operator: '!=',
+      property: ['foo'],
+      value: 'abc'
+    }).should.be.ok;
+  });
 });
